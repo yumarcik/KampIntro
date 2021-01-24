acd2ed77fee7202511fb4ed091a6e95cd120717c
Yağmur Polat <yamu_plt@hotmail.com>
2021-01-24 20:54:50 +0300

    16 Ocak 2021

---
+++ KampIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 3200000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if(dolarDun < dolarBugun) 
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            product.ProductName = "Kamera";
+        }
+
+        
+    }
+}
