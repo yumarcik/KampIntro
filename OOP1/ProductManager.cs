c20de1b4a94b7a77d7e47071e8830288ea17aac8
Yağmur Polat <yamu_plt@hotmail.com>
2021-01-26 17:11:29 +0300

    4. Gün Sonu

--- OOP1/ProductManager.cs
+++ OOP1/ProductManager.cs
@@ -8,9 +8,13 @@ namespace OOP1
     {
         public void Add(Product product)
         {
-            product.ProductName = "Kamera";
+            Console.WriteLine(product.ProductName + " eklendi.");
         }
 
-        
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+                
     }
 }
